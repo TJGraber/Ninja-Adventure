e9127077f4073e2c05bf94a8830b6f9386e0ab7f
TJGraber <grabertj@oldcolony.info>
2024-03-08 14:17:38 -0500

    Animating the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] Vector2 movementDirection;
 
+    readonly int moveX = Animator.StringToHash("moveX");
+    readonly int moveY = Animator.StringToHash("moveY");
+    readonly int isMoving = Animator.StringToHash("isMoving");
+
     PlayerActions actions;
     Rigidbody2D rb;
+    Animator anim;
 
     private void Awake()
     {
         actions = new PlayerActions();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -30,6 +37,16 @@ public class PlayerMovement : MonoBehaviour
     void ReadMovement()
     {
         movementDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+
+        if(movementDirection == Vector2.zero)
+        {
+            anim.SetBool(isMoving, false);
+            return;
+        }
+
+        anim.SetBool(isMoving, true);
+        anim.SetFloat(moveX, movementDirection.x);
+        anim.SetFloat(moveY, movementDirection.y);
     }
 
     void Move()
