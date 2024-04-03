1ecc648a53c0c962f1154c389a41d37eb3be865a
TJGraber <grabertj@oldcolony.info>
2024-04-03 12:54:21 -0400

    3.1 and part of 3.2

---
+++ Assets/Scripts/FSM_State.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class FSM_State
+{
+    public EnemyState ID;
+    public FSM_Action[] Actions;
+    public FSM_Transition[] Transitions;
+
+    void ExecuteActions()
+    {
+        for(int i = 0; i < Actions.Length; i++)
+        {
+
+            Actions[i].Act();
+        }
+    }
+
+    void ExecuteTransitions(EnemyBrain brain)
+    {
+        if (Transitions == null || Transitions.Length <= 0)
+        {
+            return;
+        }
+
+        for(int i = 0; i < Transitions.Length; i++)
+        {
+            bool value = Transitions[i].Decision.Decide();
+
+            if (value)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
